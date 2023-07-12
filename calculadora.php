c16e05b672c77f3e797715af12a0dc433e5b0d0b
andersonlevy <137961879+andersonlevy@users.noreply.github.com>
2023-07-11 21:56:30 -0300

    Create calculadora.php

---
+++ calculadora.php
@@ -0,0 +1,33 @@
+<?php
+
+// + - * /
+
+
+$value1 = "1000";
+$value2 = "10";
+$operation = "/";
+
+$result = 0;
+
+
+if ($operation == "+") {
+
+    $result = $value1 + $value2; // 20 = 10 + 10
+}
+
+if ($operation == "*") {
+
+    $result = $value1 * $value2; // 100 = 10 * 10
+}
+
+if ($operation == "-") {
+
+    $result = $value1 - $value2; // 0 = 10 - 10
+}
+
+if ($operation == "/") {
+
+    $result = $value1 / $value2; // 1 = 10 / 10
+}
+
+echo $result; // 20
